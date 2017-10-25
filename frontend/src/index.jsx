4c0430d6792164c6e3d3a4f7cd02191075315ee1
Apps RingByName <Marcelo@Appss-Mac-mini.local>
2017-10-25 15:59:10 -0200

    first commit

---
+++ frontend/src/index.jsx
@@ -0,0 +1,11 @@
+/*import React from 'react'
+import ReactDom from 'react-dom'
+import App from './main/app'
+
+ReactDom.render(<App />, docuemnt.getElementById('app'))*/
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+ReactDOM.render(<App />, document.getElementById('app'))
